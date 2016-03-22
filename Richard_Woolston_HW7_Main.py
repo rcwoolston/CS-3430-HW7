b0f10059e3af385ec36d1d4b8b85e800f40a1d4b
rcwoolston <rcwoolston@hotmail.com>
2016-03-21 21:04:49 -0600

    Initial changes

--- Richard_Woolston_HW7_Main.py
+++ Richard_Woolston_HW7_Main.py
@@ -1,18 +1,11 @@
 import os
-from os import listdir
-from os.path import isfile, join
-import sys
-from PIL import Image
-import numpy
-import glob
+import cv2
+from matplotlib import pyplot as plt
 
 # Global variables
-sharp_images = []
-blurry_images = []
-
-def filterFindEdges(image):
-    im1 = image.filter(ImageFilter.FIND_EDGES)
-    im1.save("EDGES.jpg")
+sharp_paths = []
+blurry_paths = []
+image_paths = []
 
 # Getting the path for the test pictures
 sharp_folder_path = os.path.join(os.getcwd(),'sample_images\Sample sharp images')
@@ -23,97 +16,55 @@ for f in os.listdir(sharp_folder_path):
     ext = os.path.splitext(f)[1]
     if ext.lower() not in valid_images:
         continue
-    sharp_images.append(Image.open(os.path.join(sharp_folder_path,f)))
+    sharp_paths.append(os.path.join(sharp_folder_path,f))
 
 for f in os.listdir(blurry_folder_path):
     ext = os.path.splitext(f)[1]
     if ext.lower() not in valid_images:
         continue
-    blurry_images.append(Image.open(os.path.join(blurry_folder_path,f)))
+    blurry_paths.append(os.path.join(sharp_folder_path,f))
 
+image_paths = sharp_paths + blurry_paths
 
-for i in sharp_images:
-    temp_image = numpy.array(i)
+answerDict = {}
+folders=[]
+count = 0
+for i in image_paths:
+    img = cv2.imread(i,0)
+    edges = cv2.Canny(img,100,200)
+    while 1:
+        path, folder = os.path.split(i)
 
-    thresh = 35
-    MinZero = 0.05
+        if folder != "":
+            folders.append(folder)
+        else:
+            if path != "":
+                folders.append(path)
 
-    x=numpy.asarray(temp_image)
-    x_cropped=x[0:(numpy.shape(x)[0]/16)*16 - 1,0:(numpy.shape(x)[1]/16)*16 - 1]
-    LL1,(LH1,HL1,HH1)=pywt.dwt2(x_cropped,'haar')
-    LL2,(LH2,HL2,HH2)=pywt.dwt2(LL1      ,'haar')
-    LL3,(LH3,HL3,HH3)=pywt.dwt2(LL2      ,'haar')
-    Emap1=numpy.square(LH1) + numpy.square(HL1) + numpy.square(HH1)
-    Emap2=numpy.square(LH2) + numpy.square(HL2) + numpy.square(HH2)
-    Emap3=numpy.square(LH3) + numpy.square(HL3) + numpy.square(HH3)
+            break
 
-    dimx=numpy.shape(Emap1)[0]/8
-    dimy=numpy.shape(Emap1)[1]/8
-    Emax1=[]
-    vert=1
-    for j in range(0,dimx - 2):
-        horz=1;
-        Emax1.append([])
-        for k in range(0,dimy - 2):
-            Emax1[j].append(numpy.max(numpy.max(Emap1[vert:vert+7,horz:horz+7])))
-            horz=horz+8
-        vert=vert+8
-
-    dimx=numpy.shape(Emap2)[0]/4
-    dimy=numpy.shape(Emap2)[1]/4
-    Emax2=[]
-    vert=1
-    for j in range(0,dimx - 2):
-        horz=1;
-        Emax2.append([])
-        for k in range(0,dimy - 2):
-            Emax2[j].append(numpy.max(numpy.max(Emap2[vert:vert+3,horz:horz+3])))
-            horz=horz+4
-        vert=vert+4
-
-    dimx=numpy.shape(Emap3)[0]/2
-    dimy=numpy.shape(Emap3)[1]/2
-    Emax3=[]
-    vert=1
-    for j in range(0,dimx - 2):
-        horz=1;
-        Emax3.append([])
-        for k in range(0,dimy - 2):
-            Emax3[j].append(numpy.max(numpy.max(Emap3[vert:vert+1,horz:horz+1])))
-            horz=horz+2
-        vert=vert+2
-
-    N_edge=0
-    N_da=0
-    N_rg=0
-    N_brg=0
-
-    EdgeMap = []
-    for j in range(0, dimx - 2):
-        EdgeMap.append([])
-        for k in range(0, dimy - 2):
-            if (Emax1[j][k]>thresh) or (Emax2[j][k]>thresh) or (Emax3[j][k]>thresh):
-                EdgeMap[j].append(1)
-                N_edge = N_edge + 1
-                rg = 0
-                if (Emax1[j][k]>Emax2[j][k]) and (Emax2[j][k]>Emax3[j][k]):
-                    N_da=N_da+1
-                elif (Emax1[j][k]<Emax2[j][k]) and (Emax2[j][k]<Emax3[j][k]):
-                    rg = 1
-                    N_rg=N_rg+1
-                elif (Emax2[j][k]>Emax1[j][k]) and (Emax2[j][k]>Emax3[j][k]):
-                    rg = 1
-                    N_rg=N_rg+1
-                if rg and (Emax1[j][k]<thresh):
-                    N_brg=N_brg+1
-            else:
-                EdgeMap[j].append(0)
-
-    per=float(N_da)/N_edge
-    BlurExtent=float(N_brg)/N_rg
-
-    if per>MinZero:
-        print('Not blurred')
+        folders.reverse()
+        print folders
+    if edges == None:
+        answerDict[count] = 'B'
     else:
-        print('Blurred')
-    print('BlurExtent: ' + str(BlurExtent))
+        answerDict[count] = 'S'
+
+    count+=1
+
+img = cv2.imread(sharp_paths.pop(),0)
+edges = cv2.Canny(img,100,200)
+
+temp = edges.shape
+
+img2 = cv2.imread(blurry_paths.pop(),0)
+edges = cv2.Canny(img2,100,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
+
+print 'here'
